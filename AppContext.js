c030a8e4deff2dc7fde8ddeca5e37f6bdddb4778
shortyfactory <shortyfactory@gmail.com>
2020-11-19 00:56:54 +0100

    added loging and logout and persist login from api

--- AppContext.js
+++ AppContext.js
@@ -1,6 +1,23 @@
-import Axios from 'axios';
 import React from 'react';
 import APIKit, {setClientToken} from './API';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeData =  (key, value) => {
+  try {
+     AsyncStorage.setItem(key, value)
+  } catch (e) {
+    // saving error
+  }
+}
+
+clearAll =  () => {
+  try {
+    AsyncStorage.clear()
+  } catch(e) {
+    // clear error
+  }
+}
+
 
 export const AppContext = React.createContext();
 
@@ -8,9 +25,15 @@ export const AppContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  React.useEffect(() => {
+    AsyncStorage.getItem('user').then(userString => {
+      setUser(JSON.parse(userString))
+      AsyncStorage.getItem('token').then(token => setClientToken(token));     
+    })
+  }, [])
   const login =  (email, password) => {
     setLoading(true);
-    APIKit.post('http://192.168.1.59/api/login', {
+    APIKit.post('/api/login', {
       email : "hichem@test.com",
       password : "123456",
       device_name: 'android'
@@ -20,6 +43,8 @@ export const AppContextProvider = ({ children }) => {
         setUser(response.data.user);
         setLoading(false);
         setError(null);
+        storeData('user', JSON.stringify(response.data.user));
+        storeData('token', response.data.token);
       }).catch( error => {
       console.log(error.response.data);
       setError(error.response.data);
@@ -27,7 +52,11 @@ export const AppContextProvider = ({ children }) => {
     })
   }
   const logout = async () => {
-    setUser(null);
+    APIKit.post('/api/logout').then( response => {
+      setUser(null);
+      clearAll();
+      console.log(response.data);
+    }).catch( error => console.log(error.response) );
   }
   return (
     <AppContext.Provider value={{
