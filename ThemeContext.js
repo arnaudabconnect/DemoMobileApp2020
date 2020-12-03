355fa83cf1f353ba24f9ecd64419d0aabc4ba4bc
shortyfactory <shortyfactory@gmail.com>
2020-12-04 00:39:27 +0100

    another one

--- ThemeContext.js
+++ ThemeContext.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
 export const ThemeContext = React.createContext({
@@ -7,10 +8,16 @@ export const ThemeContext = React.createContext({
 
 export const ThemeContextProvider = ({ children }) => {
   const [theme, setTheme] = React.useState('dark');
-
+  React.useEffect(() => {
+    AsyncStorage.getItem('themeChoice').then(themeChoice => {
+      setTheme(themeChoice)
+    }).catch( err => console.log(err) );
+  }, []);
+  
   const toggleTheme = () => {
     const nextTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(nextTheme);
+    AsyncStorage.setItem('themeChoice', nextTheme).then( () => console.log('theme saved') ).catch( err => console.log(err) );
   };
   return (
     <ThemeContext.Provider value={{
